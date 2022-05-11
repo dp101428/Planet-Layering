a0ee2bb77bf2159b8cd3e63b4a46adf05378fe51
Darcy Phipps <diphipps@ucsc.edu>
2022-05-10 21:11:53 -0700

    Proof of concept

--- src/prefabs/layer.js
+++ src/prefabs/layer.js
@@ -1,4 +1,4 @@
-class layer{
+class Layer{
 	constructor(scene, index, layerAbove, layerBelow, colour, quantity= 0){
 		this.scene = scene
 		this.index = index
@@ -12,23 +12,39 @@ class layer{
 
 	draw(){
 		//each shape is 10x10, grid is 30x30
-		let totalToDraw = quantity;
+		if(this.quantity != Math.floor(this.quantity))
+			return
+		let newObjects = []
+		let totalToDraw = this.quantity;
 		let sideLength = Math.floor(Math.sqrt(this.quantity))
-		if(quantity != 0){
-			objects.push(this.scene.add.rectangle(340, 60, 10 * sideLength, 10 * sideLength, this.colour))
-			this.totalToDraw -= sideLength;
+		console.log(totalToDraw)
+		console.log(sideLength);
+		if(this.quantity != 0){
+			newObjects.push(this.scene.add.rectangle(340 + sideLength * 5, 60+ sideLength * 5, 10 * sideLength, 10 * sideLength, this.colour))
+			totalToDraw -= sideLength * sideLength;
 		}
 		let i = 0
-		while (totalToDraw > 0 && i <= sideLength){
-			objects.push(this.scene.add.rectangle(340 + sideLength, 60 + 10 * i, 10, 10, this.colour))
+		while (totalToDraw > 0 && i < sideLength){
+			newObjects.push(this.scene.add.rectangle(340 + sideLength*10 +5, 60 + 10 * i +5, 10, 10, this.colour))
 			totalToDraw -= 1;
 			i += 1;
 		}
+		console.log(i)
+		console.log(totalToDraw)
 		let j = 0
-		while (totalToDraw > 0 && j <= sideLength + 1){
-			objects.push(this.scene.add.rectangle(340 + sideLength + 10 * j, 60 + sideLength, 10, 10, this.colour))
+		while (totalToDraw > 0 && j < sideLength + 1){
+			newObjects.push(this.scene.add.rectangle(340 + 10 * j + 5, 60 + sideLength * 10 +5, 10, 10, this.colour))
 			totalToDraw -= 1;
 			j += 1;
 		}
+		console.log(j)
+		console.log(totalToDraw)
+		
+		console.log("difference")
+		console.log(this.quantity - i - j - sideLength * sideLength)
+		this.objects.forEach(
+			(shape) => {shape.destroy()}
+		)
+		this.objects = newObjects;
 	}
 }
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -2,12 +2,15 @@ class Play extends Phaser.Scene {
     constructor(){
         super("playScene")    
     }
-
     create(){
-    	this.layer1 = new Layer(this, 0, null, null, new Color({r:255, g:0, b:0, a:255}), quantity= 0);
+    	this.layer1 = new Layer(this, 0, null, null, Phaser.Display.Color.GetColor(255,0,0), 1);
     }
 
     update(){
-
+    	this.layer1.draw()
+    	if(this.layer1.quantity < 900){
+    		this.layer1.quantity += 0.1
+    		this.layer1.quantity = Math.round(this.layer1.quantity * 10)/10
+    	}
     }
 }
