6dd44e2b014428431c35f1734b0dec75c9cdf935
Darcy Phipps <diphipps@ucsc.edu>
2022-05-12 16:32:43 -0700

    Fixed some text placement

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -6,7 +6,7 @@ class Play extends Phaser.Scene {
     	this.layers = []
     	this.layers.push(new Layer(this, 0, this.layers, Phaser.Display.Color.GetColor(255,0,0), 1, 
     		[{button: this.add.text(350, 100, 'Heat core', { fill: '#fff' }), effect: ()=>{this.layers[0].passiveGrowth += 0.025}, requirement: 87},
-    		{button: this.add.text(450, 150, 'Add Earth', { fill: '#fff' }), effect: ()=>{this.layers[1].quantity += 1}, requirement: 700}], 0.1))
+    		{button: this.add.text(450, 150, 'Add Earth', { fill: '#fff' }), effect: ()=>{this.layers[1].quantity += 1}, requirement: 371}], 0.1))
     	this.layers.push(new Layer(this, 1, this.layers, Phaser.Display.Color.GetColor(200,0,100), 0, [
     		{button: this.add.text(350, 100, 'Cool core', { fill: '#fff' }), effect: ()=>{this.layers[0].passiveGrowth -= 0.025,
     		 this.layers[1].passiveGrowth += 0.025}, requirement: 87}], 0))
