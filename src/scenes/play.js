03f42e6c7e487f6dba98116cda8dc4deed89d6a2
Darcy Phipps <diphipps@ucsc.edu>
2022-05-14 00:29:00 -0700

    removed playtest additions

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -4,17 +4,17 @@ class Play extends Phaser.Scene {
     }
     create(){
     	this.layers = []
-    	this.layers.push(new Layer(this, 0, this.layers, Phaser.Display.Color.GetColor(255,0,0), 380, 
+    	this.layers.push(new Layer(this, 0, this.layers, Phaser.Display.Color.GetColor(255,0,0), 1, 
     		[{button: this.add.text(350, 100, 'Heat core', { fill: '#fff' }), effect: ()=>{this.layers[0].passiveGrowth += 0.025}, requirement: 87},
     		{button: this.add.text(450, 150, 'Add Earth', { fill: '#fff' }), effect: ()=>{this.layers[1].quantity += 1}, requirement: 371}], 0.1))
-    	this.layers.push(new Layer(this, 1, this.layers, Phaser.Display.Color.GetColor(120,42,42), 70, [
+    	this.layers.push(new Layer(this, 1, this.layers, Phaser.Display.Color.GetColor(120,42,42), 0, [
     		{button: this.add.text(350, 100, 'Cool core', { fill: '#fff' }), effect: ()=>{
     			if(this.layers[0].passiveGrowth - 0.025 > 0){
     				this.layers[0].passiveGrowth -= 0.025
     		 		this.layers[1].passiveGrowth += 0.025}
     		 }, requirement: 87}, 
     		{button: this.add.text(430, 150, 'Precipitate', { fill: '#fff' }), effect: ()=>{this.layers[2].quantity += 1}, requirement: 371}], 0))
-    	this.layers.push(new Layer(this, 2, this.layers, Phaser.Display.Color.GetColor(0,0,200), 50, 
+    	this.layers.push(new Layer(this, 2, this.layers, Phaser.Display.Color.GetColor(0,0,200), 0, 
     		[{button: this.add.text(340, 100, 'Cycle water', { fill: '#fff' }), effect: ()=>{this.layers[2].passiveGrowth += 0.025}, requirement: 106}], 0))
 
     }
